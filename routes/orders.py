42d2315354933c40cb8f3bce03fc35b7a3917e6a
Kwooby <alexcasteladavis@hotmail.com>
2026-08-04 12:51:54 -0700

    Implement POST /orders endpoint

--- routes/orders.py
+++ routes/orders.py
@@ -1,9 +1,9 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, request
 from database import get_db_connection
 
 orders_bp = Blueprint("orders", __name__)
 
-@orders_bp.route('/orders')
+@orders_bp.route('/orders', methods=["GET"])
 def get_orders():
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -20,3 +20,32 @@ def get_orders():
     conn.close()
 
     return jsonify(orders)
+
+@orders_bp.route('/orders', methods=["POST"])
+def create_order():
+    data = request.get_json()
+
+    user_id = data["user_id"]
+    status = data["status"]
+    total = data["total"]
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO orders (user_id, status, total)
+        VALUES (%s, %s, %s)
+        RETURNING id;
+    """, (user_id, status, total))
+
+    order_id = cursor.fetchone()["id"]
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    
+    return jsonify({
+        "message": "Order created",
+        "order_id": order_id
+    })
